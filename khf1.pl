1b779b0d2204c80144e1439af1343325c5e19a4f
kreaturexyz <kreaturexyz@gmail.com>
2016-10-06 21:22:07 +0200

    Initial file added

---
+++ khf1.pl
@@ -0,0 +1,7 @@
+% :- type matrix == list(row).
+% :- type row == list(any).
+% :- type parameter ---> subRows-subCols.
+% :- type subRows == int.
+% :- type subCols == int.
+% :- pred feldarabolasa(+matrix, +parameter, ?list(list(any))).
+% feldarabolasa(Mx, P, LL): Az LL lista az Mx mátrix P paraméterű feldarabolása.
