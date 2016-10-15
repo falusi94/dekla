85a5b8325ae9943fddbd730e0880aa3591469617
kreaturexyz <kreaturexyz@gmail.com>
2016-10-15 13:49:07 +0200

    Working getItem, gives back list

--- khf_2_erlang/khf2.erl
+++ khf_2_erlang/khf2.erl
@@ -12,5 +12,41 @@
 %%   Vals az SSpec specifikációval megadott Sudoku-feladvány R_C
 %%   koordinátájú mezőjében megengedett értékek listája.
 
-ertekek() ->
+ertekek({K, L}, {R,C}) ->
+    RET = getItem(L,R,C),
+    io:format("RET: ~p \n", [RET]),
     [].
+
+getItem([H|T], RI, CI, R, C) ->
+    io:format("R: ~p  C: ~p  H: ~p\n", [RI, CI,H]),
+    ELEMENT_LIST = isElementList(H),
+    if
+        is_list(H) andalso ELEMENT_LIST ->
+            io:format("is_list(H) andalso ELEMENT_LIST\n", []),
+            if
+                RI<R ->
+                    getItem(T, RI+1, CI, R, C);
+                RI=:=R ->
+                    getItem(H, RI, CI, R, C)
+            end;
+        is_list(H) andalso not ELEMENT_LIST ->
+            io:format("is_list(H) andalso not ELEMENT_LIST\n", []),
+            if
+                CI<C ->
+                    getItem(T, RI, CI+1, R, C);
+                CI=:=C ->
+                    getItem(H, RI, CI, R, C)
+            end;
+        true ->
+            io:format("RET\n", []),
+            [H|T]
+    end;
+getItem([], _, _, _, _) ->
+    [].
+getItem([H|T], R, C) ->
+    getItem([H|T], 1, 1, R, C).
+
+isElementList([H|_]) ->
+    is_list(H);
+isElementList(_) ->
+    false.
