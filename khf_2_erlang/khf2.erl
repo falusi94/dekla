976abbeb34d3ebb27aa8adc89110abb8b2b5b822
kreaturexyz <kreaturexyz@gmail.com>
2016-10-15 17:43:31 +0200

    Added normalizeInput, createTuple

--- khf_2_erlang/khf2.erl
+++ khf_2_erlang/khf2.erl
@@ -13,11 +13,79 @@
 %%   koordinátájú mezőjében megengedett értékek listája.
 
 ertekek({K, LIST}, {ROW,COL}) ->
-    RET = getItem(LIST,ROW,COL),
+    RET = getItem(LIST, ROW, COL),
     io:format("RET: ~p \n", [RET]),
     io:format("List: ~p \n", [createFullList(5)]),
-    io:format("diff: ~p \n", [removeElements(createFullList(5), [2])]),
+    io:format("diff: ~p \n", [removeElements(createFullList(5), [2,3])]),
+    io:format("tuple: ~p \n", [createTuple([o])]),
+    io:format("original: ~p \n", [LIST]),
+    io:format("normalized: ~p \n", [normalizeInputList({K, LIST})]),
     [].
+
+% Change items to tuples {num, even/odd, south/west}
+normalizeInputList({K, LIST}) ->
+    normalizeInputList({K, LIST}, 1, 1, [], []).
+normalizeInputList({K, LIST}, ROWI, COLI, RET, TEMP) ->
+    if
+        ROWI>K*K ->
+            RET;
+        COLI>K*K ->
+            normalizeInputList({K, LIST}, ROWI+1, 1, lists:append(RET, [TEMP]), []);
+        true ->
+            ITEM = createTuple(getItem(LIST, ROWI, COLI)),
+            normalizeInputList({K, LIST}, ROWI, COLI+1, RET, lists:append(TEMP, [ITEM]))
+    end.
+
+% Create tuple from an 'item' (field)
+createTuple(ITEM) ->
+    PARITY = evenOrOdd(ITEM),
+    DIRECTION = southOrWest(ITEM),
+    NUMBER = number(ITEM),
+    {NUMBER, PARITY, DIRECTION}.
+
+% Gives back the number if exists in item, else -1
+number([HEAD]) ->
+    if
+        is_number(HEAD) ->
+            HEAD;
+        true ->
+            -1
+    end;
+number([HEAD|TAIL]) ->
+    if
+        is_number(HEAD) ->
+            HEAD;
+        true ->
+            number(TAIL)
+    end;
+number([]) ->
+    -1.
+
+% Gives back proper direction, if given, else return u (unknown)
+southOrWest(ITEM) ->
+    MEMBER_S = lists:member(s, ITEM),
+    MEMBER_W = lists:member(w, ITEM),
+    if
+        MEMBER_S ->
+            s;
+        MEMBER_W ->
+            w;
+        true ->
+            u
+    end.
+
+% Gives back proper parity, if given, else return u (unknown)
+evenOrOdd(ITEM) ->
+    MEMBER_O = lists:member(o, ITEM),
+    MEMBER_E = lists:member(e, ITEM),
+    if
+        MEMBER_O ->
+            o;
+        MEMBER_E ->
+            e;
+        true ->
+            u
+    end.
 
 % Remove not valid elements from list
 removeElements(LIST, [HEAD]) ->
