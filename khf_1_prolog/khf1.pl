b855e74bf808baf70529e1dfd406635da17d84c9
kreaturexyz <kreaturexyz@gmail.com>
2016-11-10 22:34:18 +0100

    Removed unnecessary makeRows #prolog1

--- khf_1_prolog/khf1.pl
+++ khf_1_prolog/khf1.pl
@@ -9,21 +9,6 @@
 feldarabolasa(MATRIX, R-C, RET) :-
     makeArrays(MATRIX, R, C, RET).
 
-makeRows(LIST, PARAM, RET) :-
-    makeRows(LIST, PARAM, 1, [], [], RET).
-makeRows([HEAD|TAIL], PARAM, INDEX, TEMP, ASD, RET) :-
-    PARAM =:= INDEX ->
-        append(TEMP, [HEAD], LIST),
-        append(ASD, [LIST], RET1),
-        makeRows(TAIL, PARAM, 1, [], RET1, RET);
-
-        append(TEMP, [HEAD], LIST),
-        INDEX1 is INDEX + 1,
-        makeRows(TAIL, PARAM, INDEX1, LIST, ASD, RET).
-makeRows([HEAD], _, _, TEMP, ASD, RET) :-
-    append(TEMP, [HEAD], TEMP1),
-    append(ASD, [TEMP1], RET).
-
 % Return an item from given matrix structure
 getItem([HEAD|TAIL], ROW, COL, RET) :-
     ROW=:=1 ->
